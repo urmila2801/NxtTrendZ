bc52adbd15eb33077ad719af4603a49adb30a347
urmila2801 <urmilapamarthi3@gmail.com>
2023-04-06 06:31:44 +0000

    practice-27

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import LoginForm from './components/LoginForm'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={LoginForm} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
